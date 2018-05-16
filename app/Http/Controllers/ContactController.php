6bf2d00bef6d7310578b8c4b7df8bb16d8b60056
ivangeorgiev97 <georgiev1313@abv.bg>
2018-05-16 11:59:25 +0300

    Update ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -24,13 +24,6 @@ class ContactController extends Controller {
             'bodyMessage' => $validated['message']
         );
 
-        Mail::send(array(), array(), function($message) use ($data) {
-            $message->from($data['email']);
-            $message->to('randomplayer1313@abv.bg');
-            $message->subject($data['subject']);
-            $message->setBody($data['bodyMessage']);
-        });
-
         return response()->json($this->contactService->sendEmail($data));
     }
 
